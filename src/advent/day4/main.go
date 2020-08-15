9f722d1816635975848991807fc66b90b55dbb58
Rok Povsic <rok.povsic@gmail.com>
2020-08-15 22:27:31 +0200

    Add day 4 - part 2

--- src/advent/day4/main.go
+++ src/advent/day4/main.go
@@ -9,16 +9,21 @@ func Run() {
 	from := 240920
 	to := 789857
 
-	count := 0
+	countPart1 := 0
+	countPart2 := 0
 	for num := from; num <= to; num++ {
-		if isValid(num) {
-			count++
+		if isValidForPart1(num) {
+			countPart1++
+		}
+		if isValidForPart2(num) {
+			countPart2++
 		}
 	}
-	fmt.Println(count)
+	fmt.Printf("Part 1: %d\n", countPart1)
+	fmt.Printf("Part 2: %d\n", countPart2)
 }
 
-func isValid(num int) bool {
+func isValidForPart1(num int) bool {
 	str := strconv.Itoa(num)
 	isDuplicated := false
 	for i := 0; i < 5; i++ {
@@ -34,6 +39,32 @@ func isValid(num int) bool {
 		}
 	}
 	return isDuplicated
+}
+
+func isValidForPart2(num int) bool {
+	str := strconv.Itoa(num)
+	for i := 0; i < 5; i++ {
+		cur, err := strconv.Atoi(str[i : i+1])
+		check(err)
+		next, err := strconv.Atoi(str[i+1 : i+2])
+		check(err)
+		if cur > next {
+			return false
+		}
+	}
+
+	var m = make(map[int]int)
+	for i := 0; i < 6; i++ {
+		cur, err := strconv.Atoi(str[i : i+1])
+		check(err)
+		m[cur]++
+	}
+	for _, v := range m {
+		if v == 2 {
+			return true
+		}
+	}
+	return false
 }
 
 func check(e error) {
