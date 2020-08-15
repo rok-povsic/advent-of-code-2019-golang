fad6b67147cb71e84c44877fff7b1b34b79510b4
Rok Povsic <rok.povsic@gmail.com>
2020-08-15 21:31:55 +0200

    Add day 3 - part 2

--- src/advent/day3/day3_test.go
+++ src/advent/day3/day3_test.go
@@ -18,3 +18,20 @@ func TestNearestManhattanDistance(t *testing.T) {
 		t.Errorf("Fail: %d", distance)
 	}
 }
+
+func TestNearestSignalDistance(t *testing.T) {
+	distance := NearestSignalDistance("R8,U5,L5,D3\nU7,R6,D4,L4")
+	if distance != 30 {
+		t.Errorf("Fail: %d", distance)
+	}
+
+	distance = NearestSignalDistance("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83")
+	if distance != 610 {
+		t.Errorf("Fail: %d", distance)
+	}
+
+	distance = NearestSignalDistance("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+	if distance != 410 {
+		t.Errorf("Fail: %d", distance)
+	}
+}
