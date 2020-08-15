909f68fddd6a42114acd1be0e7cfe33ef3758a03
Rok Povsic <rok.povsic@gmail.com>
2020-08-15 14:51:05 +0200

    Add day 2 - part 2

--- src/advent/day2/day2.go
+++ src/advent/day2/day2.go
@@ -7,11 +7,25 @@ import (
 )
 
 func Run() {
-	fmt.Println(Compute("1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,1,9,27,31,1,31,10,35,2,13,35,39,1,39,10,43,1,43,9,47,1,47,13,51,1,51,13,55,2,55,6,59,1,59,5,63,2,10,63,67,1,67,9,71,1,71,13,75,1,6,75,79,1,10,79,83,2,9,83,87,1,87,5,91,2,91,9,95,1,6,95,99,1,99,5,103,2,103,10,107,1,107,6,111,2,9,111,115,2,9,115,119,2,13,119,123,1,123,9,127,1,5,127,131,1,131,2,135,1,135,6,0,99,2,0,14,0"))
+	program := "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,1,9,27,31,1,31,10,35,2,13,35,39,1,39,10,43,1,43,9,47,1,47,13,51,1,51,13,55,2,55,6,59,1,59,5,63,2,10,63,67,1,67,9,71,1,71,13,75,1,6,75,79,1,10,79,83,2,9,83,87,1,87,5,91,2,91,9,95,1,6,95,99,1,99,5,103,2,103,10,107,1,107,6,111,2,9,111,115,2,9,115,119,2,13,119,123,1,123,9,127,1,5,127,131,1,131,2,135,1,135,6,0,99,2,0,14,0"
+	L: for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			result := Compute(noun, verb, program)
+			output := strings.Split(result, ",")[0]
+			if output == "19690720" {
+				fmt.Printf("noun = %d, verb = %d\n", noun, verb)
+				fmt.Printf("100 * noun + verb = %d\n", 100 * noun + verb)
+				break L
+			}
+		}
+	}
 }
 
-func Compute(text string) string {
+func Compute(noun int, verb int, text string) string {
 	instrs := instrs(text)
+
+	instrs[1] = noun
+	instrs[2] = verb
 
 	pc := 0
 
@@ -27,7 +41,6 @@ func Compute(text string) string {
 			b := instrs[bAddr]
 			c := a + b
 			instrs[cAddr] = c
-			fmt.Printf("%d + %d and store to %d\n", a, b, cAddr)
 			pc += 4
 		}
 		case 2: {
@@ -38,11 +51,9 @@ func Compute(text string) string {
 			b := instrs[bAddr]
 			c := a * b
 			instrs[cAddr] = c
-			fmt.Printf("%d * %d and store to %d\n", a, b, cAddr)
 			pc += 4
 		}
 		case 99: {
-			fmt.Println("Exit")
 			break L
 		}
 		default:
--- src/advent/day2/day2_test.go
+++ src/advent/day2/day2_test.go
@@ -3,27 +3,27 @@ package day2
 import "testing"
 
 func TestIntcode(t *testing.T) {
-	result := Compute("1,9,10,3,2,3,11,0,99,30,40,50")
+	result := Compute(0, 0, "1,9,10,3,2,3,11,0,99,30,40,50")
 	if result != "3500,9,10,70,2,3,11,0,99,30,40,50" {
 		t.Errorf("Fail %s", result)
 	}
 
-	result = Compute("1,0,0,0,99")
+	result = Compute(0, 0, "1,0,0,0,99")
 	if result != "2,0,0,0,99" {
 		t.Errorf("Fail %s", result)
 	}
 
-	result = Compute("2,3,0,3,99")
+	result = Compute(0, 0, "2,3,0,3,99")
 	if result != "2,3,0,6,99" {
 		t.Errorf("Fail %s", result)
 	}
 
-	result = Compute("2,4,4,5,99,0")
+	result = Compute(0, 0, "2,4,4,5,99,0")
 	if result != "2,4,4,5,99,9801" {
 		t.Errorf("Fail %s", result)
 	}
 
-	result = Compute("1,1,1,4,99,5,6,0,99")
+	result = Compute(0, 0, "1,1,1,4,99,5,6,0,99")
 	if result != "30,1,1,4,2,5,6,0,99" {
 		t.Errorf("Fail %s", result)
 	}
