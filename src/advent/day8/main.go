b3f9ac909016788fc552a7607e37973ac303514a
Rok Povsic <rok.povsic@gmail.com>
2020-10-03 21:35:19 +0200

    Add day 8 - part 2

--- src/advent/day8/main.go
+++ src/advent/day8/main.go
@@ -12,6 +12,43 @@ func Run() {
 
 	text := string(dat)
 	fmt.Println(Compute(text, 25, 6))
+	Draw(text, 25, 6)
+}
+
+func Draw(text string, wide int, tall int) {
+	digits := textToDigits(text)
+
+	layerLength := wide * tall
+
+	drawing := make([]int, 0)
+	for i := 0; i < layerLength; i++ {
+		drawing = append(drawing, 2)
+	}
+
+	numLayers := len(digits) / layerLength
+	for layerIndex := numLayers - 1; layerIndex >= 0; layerIndex-- {
+		from := layerIndex * layerLength
+		to := (layerIndex + 1) * layerLength
+		layer := digits[from:to]
+
+		for i := 0; i < layerLength; i++ {
+			if layer[i] != 2 {
+				drawing[i] = layer[i]
+			}
+		}
+	}
+
+	for i := 0; i < layerLength; i++ {
+		if drawing[i] == 0 {
+			fmt.Print(" ")
+		} else {
+			fmt.Print(drawing[i])
+		}
+
+		if i != 0 && (i + 1) % wide == 0 {
+			fmt.Println()
+		}
+	}
 }
 
 func Compute(text string, wide int, tall int) int {
