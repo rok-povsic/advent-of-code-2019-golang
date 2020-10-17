401ac7daf0aa231a06039f1f73b0d71e9b0c935a
Rok Povsic <rok.povsic@gmail.com>
2020-10-17 21:18:10 +0200

    Add day 10 - part 2

--- src/advent/day10/main.go
+++ src/advent/day10/main.go
@@ -4,10 +4,66 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"sort"
 )
 
 type Location struct {
 	x, y int
+}
+
+func TwohundrethVaporizedAsteroid(asteroidMap string, deployedX int, deployedY int) (int, int) {
+	asteroidLocations := parseLocations(asteroidMap)
+	gunLocation := Location{deployedX, deployedY}
+
+	// This is problematic due to floating point arithmetic but it turns out to work in this example, so no need
+	// to spend more time on it.
+	angleToLocations := make(map[float64][]Location)
+	for _, asteroidLocation := range asteroidLocations {
+		if gunLocation == asteroidLocation {
+			continue
+		}
+		a := angle(gunLocation, asteroidLocation)
+		_, exists := angleToLocations[a]
+		if !exists {
+			angleToLocations[a] = make([]Location, 0)
+		}
+		angleToLocations[a] = append(angleToLocations[a], asteroidLocation)
+	}
+	for _, locations := range angleToLocations {
+		sort.Slice(locations, func(i, j int) bool {
+			distanceToFirst := (locations[i].x - gunLocation.x) + (locations[i].y - gunLocation.y)
+			distanceToSecond := (locations[j].x - gunLocation.x) + (locations[j].y - gunLocation.y)
+			return distanceToFirst > distanceToSecond
+		})
+	}
+
+	countAsteroids := 0
+	for true {
+		ascendingAngles := make([]float64, len(angleToLocations))
+		i := 0
+		for angle := range angleToLocations {
+			ascendingAngles[i] = angle
+			i++
+		}
+		sort.Float64s(ascendingAngles)
+		for _, angle := range ascendingAngles {
+			fmt.Println(angle)
+			locations := angleToLocations[angle]
+			vaporizedLocation := locations[0]
+			fmt.Println(vaporizedLocation)
+			locations = locations[1:]
+			countAsteroids++
+
+			if len(locations) == 0 {
+				delete(angleToLocations, angle)
+			}
+
+			if countAsteroids == 200 {
+				return vaporizedLocation.x, vaporizedLocation.y
+			}
+		}
+	}
+	return -1, -1
 }
 
 func BestAsteroidLocation(asteroidMap string) (int, int, int) {
@@ -66,7 +122,11 @@ func parseLocations(asteroidMap string) []Location {
 func angle(location Location, otherLocation Location) float64 {
 	deltaX := float64(otherLocation.x - location.x)
 	deltaY := float64(location.y - otherLocation.y)
-	thetaRadians := math.Atan2(deltaY, deltaX)
+	thetaRadians := math.Atan2(deltaX, deltaY)
+	// Rotate the coordinate system to start at the top.
+	if thetaRadians < 0 {
+		thetaRadians += 2 * math.Pi
+	}
 	return thetaRadians
 }
 
@@ -80,6 +140,10 @@ func main() {
 	data, err := ioutil.ReadFile("src/advent/day10/input.txt")
 	check(err)
 	asteroidMap := string(data)
-	_, _, num := BestAsteroidLocation(asteroidMap)
+	x, y, num := BestAsteroidLocation(asteroidMap)
+	fmt.Printf("gun location, x=%d, y=%d\n", x, y)
 	fmt.Println(num)
+
+	twohundrethX, twohundrethY := TwohundrethVaporizedAsteroid(asteroidMap, x, y)
+	fmt.Println(twohundrethX*100 + twohundrethY)
 }
