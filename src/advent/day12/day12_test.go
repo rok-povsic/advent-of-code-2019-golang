65439962cb8447e39d68971dc0fa2dc7251b44e4
Rok Povsic <rok.povsic@gmail.com>
2020-10-18 14:37:19 +0200

    Add day 12 - part 2

--- src/advent/day12/day12_test.go
+++ src/advent/day12/day12_test.go
@@ -5,27 +5,51 @@ import (
 )
 
 func TestSample1(t *testing.T) {
-	moons := []Moon{
-		MakeMoon(-1, 0, 2),
-		MakeMoon(2, -10, -7),
-		MakeMoon(4, -8, 8),
-		MakeMoon(3, 5, -1),
-	}
+	moons := moons1()
 	totalEnergy := Simulate(moons, 10)
 	if totalEnergy != 179 {
 		t.Errorf("Unexpected total energy: %d\n", totalEnergy)
 	}
 }
 
+func TestRepeatStepsSample1(t *testing.T) {
+	steps := RepeatSteps(moons1(), moons1())
+	if steps != 2772 {
+		t.Errorf("Unexpected steps: %d\n", steps)
+	}
+}
+
 func TestSample2(t *testing.T) {
+	moons := moons2()
+	totalEnergy := Simulate(moons, 100)
+	if totalEnergy != 1940 {
+		t.Errorf("Unexpected total energy: %d\n", totalEnergy)
+	}
+}
+
+func TestRepeatStepsSample2(t *testing.T) {
+	steps := RepeatSteps(moons2(), moons2())
+	if steps != 4686774924 {
+		t.Errorf("Unexpected steps: %d\n", steps)
+	}
+}
+
+func moons1() []Moon {
+	moons := []Moon{
+		MakeMoon(-1, 0, 2),
+		MakeMoon(2, -10, -7),
+		MakeMoon(4, -8, 8),
+		MakeMoon(3, 5, -1),
+	}
+	return moons
+}
+
+func moons2() []Moon {
 	moons := []Moon{
 		MakeMoon(-8, -10, 0),
 		MakeMoon(5, 5, 10),
 		MakeMoon(2, -7, 3),
 		MakeMoon(9, -8, -3),
 	}
-	totalEnergy := Simulate(moons, 100)
-	if totalEnergy != 1940 {
-		t.Errorf("Unexpected total energy: %d\n", totalEnergy)
-	}
+	return moons
 }
--- src/advent/day12/main.go
+++ src/advent/day12/main.go
@@ -23,9 +23,9 @@ func Simulate(moons []Moon, steps int) int {
 	for step := 0; step < steps; step++ {
 		fmt.Printf("After %d steps:\n", step)
 
-		printState(moons, steps)
-		updateGravity(moons)
-		applyVelocity(moons)
+		stepX(moons)
+		stepY(moons)
+		stepZ(moons)
 	}
 
 	fmt.Printf("After %d steps:\n", steps)
@@ -34,12 +34,69 @@ func Simulate(moons []Moon, steps int) int {
 	return totalEnergy(moons)
 }
 
-func printState(moons []Moon, steps int) {
-	for _, moon := range moons {
-		fmt.Println(moon)
+func RepeatSteps(initialMoons []Moon, moons []Moon) int {
+	stepsX := 1
+	for true {
+		stepX(moons)
+
+		equal := true
+		for i := 0; i < len(moons); i++ {
+			if moons[i].position.x != initialMoons[i].position.x || moons[i].velocity.x != initialMoons[i].velocity.x {
+				equal = false
+				break
+			}
+		}
+		if equal {
+			break
+		}
+		stepsX++
 	}
+
+	stepsY := 1
+	for true {
+		stepY(moons)
+
+		equal := true
+		for i := 0; i < len(moons); i++ {
+			if moons[i].position.y != initialMoons[i].position.y || moons[i].velocity.y != initialMoons[i].velocity.y {
+				equal = false
+				break
+			}
+		}
+		if equal {
+			break
+		}
+		stepsY++
+	}
+
+	stepsZ := 1
+	for true {
+		stepZ(moons)
+
+		equal := true
+		for i := 0; i < len(moons); i++ {
+			if moons[i].position.z != initialMoons[i].position.z || moons[i].velocity.z != initialMoons[i].velocity.z {
+				equal = false
+				break
+			}
+		}
+		if equal {
+			break
+		}
+		stepsZ++
+	}
+
+	fmt.Println(stepsX)
+	fmt.Println(stepsY)
+	fmt.Println(stepsZ)
+
+	fmt.Printf("After %d steps:\n", stepsX)
+	steps := lcm(stepsX, stepsY, stepsZ)
+	printState(moons, steps)
+	return steps
 }
-func updateGravity(moons []Moon) {
+
+func stepX(moons []Moon) {
 	for i := 0; i < len(moons)-1; i++ {
 		for j := i + 1; j < len(moons); j++ {
 			if moons[i].position.x < moons[j].position.x {
@@ -49,7 +106,17 @@ func updateGravity(moons []Moon) {
 				moons[i].velocity.x--
 				moons[j].velocity.x++
 			}
+		}
+	}
 
+	for i := 0; i < len(moons); i++ {
+		moons[i].position.x += moons[i].velocity.x
+	}
+}
+
+func stepY(moons []Moon) {
+	for i := 0; i < len(moons)-1; i++ {
+		for j := i + 1; j < len(moons); j++ {
 			if moons[i].position.y < moons[j].position.y {
 				moons[i].velocity.y++
 				moons[j].velocity.y--
@@ -57,7 +124,17 @@ func updateGravity(moons []Moon) {
 				moons[i].velocity.y--
 				moons[j].velocity.y++
 			}
+		}
+	}
 
+	for i := 0; i < len(moons); i++ {
+		moons[i].position.y += moons[i].velocity.y
+	}
+}
+
+func stepZ(moons []Moon) {
+	for i := 0; i < len(moons)-1; i++ {
+		for j := i + 1; j < len(moons); j++ {
 			if moons[i].position.z < moons[j].position.z {
 				moons[i].velocity.z++
 				moons[j].velocity.z--
@@ -67,16 +144,17 @@ func updateGravity(moons []Moon) {
 			}
 		}
 	}
-}
 
-func applyVelocity(moons []Moon) {
 	for i := 0; i < len(moons); i++ {
-		moons[i].position.x += moons[i].velocity.x
-		moons[i].position.y += moons[i].velocity.y
 		moons[i].position.z += moons[i].velocity.z
 	}
 }
 
+func printState(moons []Moon, steps int) {
+	for _, moon := range moons {
+		fmt.Println(moon)
+	}
+}
 func totalEnergy(moons []Moon) int {
 	result := 0
 	for _, moon := range moons {
@@ -94,13 +172,45 @@ func abs(num int) int {
 	return num
 }
 
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func lcm(a, b int, integers ...int) int {
+	result := a * b / gcd(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = lcm(result, integers[i])
+	}
+
+	return result
+}
+
 func main() {
+	initialMoons := []Moon{
+		MakeMoon(17, -12, 13),
+		MakeMoon(2, 1, 1),
+		MakeMoon(-1, -17, 7),
+		MakeMoon(12, -14, 18),
+	}
+
 	moons := []Moon{
 		MakeMoon(17, -12, 13),
 		MakeMoon(2, 1, 1),
 		MakeMoon(-1, -17, 7),
 		MakeMoon(12, -14, 18),
 	}
-	totalEnergy := Simulate(moons, 1000)
-	fmt.Printf("Total energy: %d\n", totalEnergy)
+	//totalEnergy := Simulate(moons, 1000)
+	//fmt.Printf("Total energy: %d\n", totalEnergy)
+	//
+	//fmt.Println(initialMoons)
+	//fmt.Println(moons)
+
+	steps := RepeatSteps(initialMoons, moons)
+	fmt.Printf("Number of steps: %d\n", steps)
 }
